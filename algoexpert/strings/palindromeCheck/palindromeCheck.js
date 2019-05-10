8973280eadc5dc5633429a8008eb2a6031cc1119
dwyfrequency <jdwy215@gmail.com>
2019-05-10 14:22:48 -0400

    adding test cases and initial solution code

---
+++ algoexpert/strings/palindromeCheck/palindromeCheck.js
@@ -0,0 +1,17 @@
+const palindromeCheck = str => {
+  if (!str) {
+    return false;
+  }
+  const lowerStr = str.toLowerCase();
+  const midpoint = Math.floor(lowerStr.length);
+  for (let i = 0, right = lowerStr.length - 1; i < midpoint; i++) {
+    if (lowerStr[i] !== lowerStr[right - i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+module.exports = {
+  palindromeCheck,
+};
---
+++ algoexpert/strings/palindromeCheck/palindromeCheck.test.js
@@ -0,0 +1,25 @@
+const { palindromeCheck } = require('./palindromeCheck');
+
+test('passed an empty string should return false', () => {
+  expect(palindromeCheck('')).toBe(false);
+});
+
+test('passed a non-palindrome string should return false', () => {
+  expect(palindromeCheck('blue')).toBe(false);
+});
+
+test('passed a non-palindrome string should return false', () => {
+  expect(palindromeCheck('banana')).toBe(false);
+});
+
+test('passed a palindrome string should return true', () => {
+  expect(palindromeCheck('racecar')).toBe(true);
+});
+
+test('passed a palindrome string should return true', () => {
+  expect(palindromeCheck('racecar')).toBe(true);
+});
+
+test('passed a palindrome string should return true even with capital letters mixed', () => {
+  expect(palindromeCheck('racECar')).toBe(true);
+});
