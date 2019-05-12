e84b70d2fe3347e748f5df91cbe98649e8a76ada
dwyfrequency <jdwy215@gmail.com>
2019-05-12 11:32:12 -0400

    bubblesort completed code with test spec

---
+++ algoexpert/sorting/bubbleSort/bubbleSort.js
@@ -0,0 +1,33 @@
+const bubbleSort = arr => {
+  if (!arr) return arr;
+  let left = 0,
+    right = 1,
+    passes = 0;
+
+  /* we know the last index of the array will be the largest on each pass through so
+  we can exclude it on our next sort  */
+  while (right < arr.length - passes) {
+    if (arr[left] > arr[right]) {
+      const tempVal = arr[left];
+      arr[left] = arr[right];
+      arr[right] = tempVal;
+    }
+
+    // have we hit the end of the array for the idxs we care about
+    if (right === arr.length - 1 - passes) {
+      left = 0;
+      right = 1;
+      passes++;
+    } else {
+      left++;
+      right++;
+    }
+  }
+  return arr;
+};
+
+console.log(bubbleSort([1, 4, 3, 2]));
+
+module.exports = {
+  bubbleSort,
+};
---
+++ algoexpert/sorting/bubbleSort/bubbleSort.test.js
@@ -0,0 +1,21 @@
+const { bubbleSort } = require('./bubbleSort.js');
+
+test('should return same array if less than 2 elements', () => {
+  expect(bubbleSort([1])).toEqual([1]);
+});
+
+test('should return same array if less than 2 elements', () => {
+  expect(bubbleSort([])).toEqual([]);
+});
+
+test('should return sorted array', () => {
+  expect(bubbleSort([3, 2])).toEqual([2, 3]);
+});
+
+test('should return sorted array', () => {
+  expect(bubbleSort([1, 4, 3, 2])).toEqual([1, 2, 3, 4]);
+});
+
+test('should return sorted array', () => {
+  expect(bubbleSort([1, 4, 3, -4, 2])).toEqual([-4, 1, 2, 3, 4]);
+});
