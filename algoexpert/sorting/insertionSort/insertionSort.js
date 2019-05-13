6e0ff01641b7010c11a463811182779ed790adbf
dwyfrequency <jdwy215@gmail.com>
2019-05-13 09:53:38 -0400

    initial files for insertionSort

---
+++ algoexpert/sorting/insertionSort/insertionSort.js
@@ -0,0 +1,13 @@
+const insertionSort = arr => {
+  if (!arr.length) return arr;
+
+  let sorted = false;
+  const newArr = [];
+  while (!sorted) {
+    for (let i = 0; i < arr.length; i++) {}
+  }
+};
+
+module.exports = {
+  insertionSort,
+};
