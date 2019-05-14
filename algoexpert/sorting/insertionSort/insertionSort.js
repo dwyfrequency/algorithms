2a88bcc1c058199b50cdfa06a14d453243b844d7
dwyfrequency <jdwy215@gmail.com>
2019-05-14 19:49:48 -0400

    adding some logic for dividing arr

--- algoexpert/sorting/insertionSort/insertionSort.js
+++ algoexpert/sorting/insertionSort/insertionSort.js
@@ -4,6 +4,9 @@ const insertionSort = arr => {
   let sorted = false;
   const newArr = [];
   while (!sorted) {
+    const half = Math.ceil(arr.length / 2);
+    const arr1 = arr.slice(0, half),
+      arr2 = arr.slice(half);
     for (let i = 0; i < arr.length; i++) {}
   }
 };
