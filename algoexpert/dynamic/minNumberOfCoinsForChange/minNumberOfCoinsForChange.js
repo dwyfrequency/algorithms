0e9c488c11c7d9ab5f74a5ef979eadd3d214fbd1
dwyfrequency <jdwy215@gmail.com>
2019-06-03 10:24:33 -0400

    adding solution code with notes

--- algoexpert/dynamic/minNumberOfCoinsForChange/minNumberOfCoinsForChange.js
+++ algoexpert/dynamic/minNumberOfCoinsForChange/minNumberOfCoinsForChange.js
@@ -1,22 +1,25 @@
-function minNumberOfCoinsForChange(n, denoms) {
-  // Write your code here.
-  denoms.sort((a, b) => a - b);
-  const arr = Array.from({ length: n + 1 });
-  for (let denomIdx = 0; denomIdx < denoms.length; denomIdx++) {
-    // const element = array[i];
-    for (let arrIdx = 0; arrIdx < arr.length; arrIdx++) {
-      const floor = Math.floor(arrIdx / denoms[denomIdx]);
-      const remain = arrIdx - floor * denoms[denomIdx];
-      const cnt = floor + (arr[remain] ? arr[remain] : 0);
-      if (!arr[arrIdx]) {
-        arr[arrIdx] = cnt;
-      } else if (arr[arrIdx] > cnt) {
-        arr[arrIdx] = arr[arrIdx] > cnt ? cnt : arr[arrIdx];
+// Time O(N * D) | Space O(N)
+// N is the target and D is the len of the denomArr
+const minNumberOfCoinsForChangeAlgoExp = (target, denomArr) => {
+  const coinsPerValueArr = Array.from({ length: target + 1 }, () => Infinity);
+  coinsPerValueArr[0] = 0;
+  for (let denom of denomArr) {
+    for (
+      let coinCntIndex = 1;
+      coinCntIndex < coinsPerValueArr.length;
+      coinCntIndex++
+    ) {
+      if (denom <= coinCntIndex) {
+        const dif = coinCntIndex - denom;
+        coinsPerValueArr[coinCntIndex] = Math.min(
+          coinsPerValueArr[coinCntIndex],
+          1 + coinsPerValueArr[dif]
+        );
       }
     }
   }
-  return arr[n];
-}
+  return coinsPerValueArr[target] === Infinity ? -1 : coinsPerValueArr[target];
+};
 
 console.log(minNumberOfCoinsForChange(7, [1, 5, 10]));
 
