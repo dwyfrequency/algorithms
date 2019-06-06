046267e57c2bbe3f83b5305177b3588850d3f12a
dwyfrequency <jdwy215@gmail.com>
2019-06-06 11:17:26 -0400

    adding solution code

--- algoexpert/bst/bstConstruction/bstConstruction.js
+++ algoexpert/bst/bstConstruction/bstConstruction.js
@@ -13,11 +13,21 @@ class BST {
     // Write your code here.
     // Do not edit the return statement of this method.
     let currentNode = this;
+    const insertNode = new BST(value);
     while (currentNode) {
-      currentNode =
-        currentNode.value > value ? currentNode.left : currentNode.right;
+      if (currentNode.value > value) {
+        if (!currentNode.left) {
+          currentNode.left = insertNode;
+          break;
+        }
+        currentNode = currentNode.left;
+      } else if (!currentNode.right) {
+        currentNode.right = insertNode;
+        break;
+      } else {
+        currentNode = currentNode.right;
+      }
     }
-    currentNode = new BST(value);
     return this;
   }
 
@@ -26,32 +36,78 @@ class BST {
     let currentNode = this;
     while (currentNode) {
       if (currentNode.value === value) return true;
-      currentNode =
-        currentNode.value > value ? currentNode.left : currentNode.right;
+      if (currentNode.value > value) {
+        currentNode = currentNode.left;
+      } else {
+        currentNode = currentNode.right;
+      }
     }
     return false;
   }
 
+  getMinValue(node) {
+    while (node.left) node = node.left;
+    return node.value;
+  }
+
+  removeNodeBindings(node) {
+    node.value = null;
+    node.left = null;
+    node.right = null;
+  }
+
   // eslint-disable-next-line complexity
-  remove(value) {
+  remove(value, parentNode = null) {
     // Write your code here.
     // Do not edit the return statement of this method.
     let currentNode = this;
-    let parentNode;
-    while (currentNode.value !== value) {
-      parentNode = currentNode;
-      currentNode =
-        currentNode.value > value ? currentNode.left : currentNode.right;
-    }
-    if (currentNode.left && currentNode.right) {
-      let nodeToReplace = currentNode;
-      while (currentNode.right) {
+    while (currentNode) {
+      if (currentNode.value > value) {
+        parentNode = currentNode;
+        currentNode = currentNode.left;
+      } else if (currentNode.value < value) {
+        parentNode = currentNode;
         currentNode = currentNode.right;
+      } else {
+        // found the node
+        if (currentNode.left && currentNode.right) {
+          // we need to get the left most value value on the right branch
+          currentNode.value = this.getMinValue(currentNode.right);
+          currentNode.right.remove(currentNode.value, currentNode);
+        } else if (!parentNode) {
+          if (currentNode.left) {
+            currentNode.value = currentNode.left.value;
+            currentNode.right = currentNode.left.right;
+            currentNode.left = currentNode.left.left;
+            currentNode.left.remove(currentNode.value, null);
+          } else if (currentNode.right) {
+            // we need to get the left most value value on the right branch
+            currentNode.value = currentNode.right.value;
+            currentNode.left = currentNode.right.left;
+            currentNode.right = currentNode.right.right;
+            currentNode.right.remove(currentNode.value, null);
+          }
+          return this;
+        } else if (parentNode.left === currentNode) {
+          /*
+						6
+					 /
+				  3
+				 /  \
+				2	   5
+							*/
+          parentNode.left = currentNode.left
+            ? currentNode.left
+            : currentNode.right;
+          currentNode = null;
+        } else {
+          parentNode.right = currentNode.right
+            ? currentNode.right
+            : currentNode.left;
+          currentNode = null;
+        }
+        return this;
       }
-      nodeToReplace = currentNode.value;
-      currentNode = null;
-    } else if (currentNode.left) {
-      currentNode = currentNode.left;
     }
     return this;
   }
