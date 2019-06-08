e63de9f7bc42281f4fd358b8531de2155b99e736
dwyfrequency <jdwy215@gmail.com>
2019-06-07 20:05:41 -0400

    adding second test case

--- leetcode/strings/findAnagrams/findAnagrams.js
+++ leetcode/strings/findAnagrams/findAnagrams.js
@@ -33,11 +33,44 @@ var findAnagrams = function(s, p) {
         startPointer++;
       }
     } else if (!p.includes(s[rightPointer])) {
-      startPointer += rightPointer + 1;
-      rightPointer += startPointer;
+      startPointer++;
+      rightPointer += 1;
     }
   }
   return startIdxArr;
 };
 
-console.log(findAnagrams('cbaebabacd', 'abc'));
+var findAnagramsIter = function(
+  s,
+  p,
+  leftPointer = 0,
+  rightPointer = 1,
+  startIdxArr = []
+) {
+  while (rightPointer < s.length) {
+    const lenDif = rightPointer - leftPointer;
+    if (lenDif < p.length) {
+      rightPointer++;
+    } else if (lenDif === p.length) {
+      const potentialAnagram = s.slice(leftPointer, rightPointer);
+      let match = true;
+      for (const char of p) {
+        if (!potentialAnagram.includes(char)) {
+          match = false;
+          break;
+        }
+      }
+      if (match) {
+        startIdxArr.push(leftPointer);
+      }
+      ++leftPointer;
+      ++rightPointer;
+    } else {
+      leftPointer++;
+    }
+  }
+  return startIdxArr;
+};
+
+console.log(findAnagramsIter('cbaebabacd', 'abc')); // [0, 6]
+console.log(findAnagramsIter('acdcaeccde', 'c'));
