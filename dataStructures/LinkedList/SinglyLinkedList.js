d963980c05355b062fa2959ee46ba21c81d02376
dwyfrequency <jdwy215@gmail.com>
2019-06-08 16:59:48 -0400

    adding skeleton for singly linked list

---
+++ dataStructures/LinkedList/SinglyLinkedList.js
@@ -0,0 +1,20 @@
+class Node {
+  constructor(value) {
+    this.prev = null;
+    this.next = null;
+    this.value = value;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+  setHead() {}
+  setTail() {}
+  insert() {}
+  remove() {}
+  contains() {}
+  insertAtPosition(position, nodeToInsert) {}
+}
