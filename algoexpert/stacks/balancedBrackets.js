aa81c727f3818d57c40f86e36f25370a1c84e60b
dwyfrequency <jdwy215@gmail.com>
2019-06-09 16:17:46 -0400

    solution with faster shortcircuting

--- algoexpert/stacks/balancedBrackets.js
+++ algoexpert/stacks/balancedBrackets.js
@@ -1,4 +1,4 @@
-function balancedBrackets(string) {
+function balancedBracketsMe(string) {
   // Write your code here.
   // 	Example {[]}
   const bracketMatchObj = {
@@ -20,4 +20,37 @@ function balancedBrackets(string) {
   return stack.length === 0;
 }
 
-console.log(balancedBrackets('(()())((()()()))')); // true
+// Time O(N) | Space O(N)
+function balancedBracketsAlgoExp(string) {
+  // Write your code here.
+  // 	Example {[]}
+  const openingBrackets = '[{(';
+  const closingBrackets = ']})';
+  const bracketMatchObj = {
+    '}': '{',
+    ']': '[',
+    ')': '(',
+  };
+  const stack = [];
+  for (const char of string) {
+    // is it an opening bracket, ok push it to the stack
+    if (openingBrackets.includes(char)) {
+      stack.push(char);
+      // is it a closing bracket, check if there is anything in the stack - no return false because it's unbalanced ex ']'
+    } else if (closingBrackets.includes(char)) {
+      if (!stack.length) {
+        return false;
+        // does the closing bracket, match the result from the hash table - ok pop off the the value and dont add the current
+      } else if (stack[stack.length - 1] === bracketMatchObj[char]) {
+        stack.pop();
+      }
+      // else it doesnt match and is therefore unbalanced
+      else {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+}
+
+console.log(balancedBrackets('(()())((()()()))'));
