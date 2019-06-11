d31e9399444d8ed3a33ece94e17e603e5932270c
dwyfrequency <jdwy215@gmail.com>
2019-06-11 09:12:00 -0400

    adding prompt url and notes on solution code

--- hackerrank/warmups/jumpingOnClouds/jumpingOnClouds.js
+++ hackerrank/warmups/jumpingOnClouds/jumpingOnClouds.js
@@ -1,7 +1,10 @@
+// Algo Prompt: https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
 function jumpingOnClouds(arr) {
   let cnt = 0;
-  //
+  // we do arr.length - 1 b/c we want to stop counting when we reach the last idx
   for (let i = 0; i < arr.length - 1; i++) {
+    // if i + 2 === 0, we can advance two spaces. We increment +1 in loop b/c
+    // the for loop will increment it again giving us +2
     if (arr[i + 2] === 0) {
       i++;
     }
