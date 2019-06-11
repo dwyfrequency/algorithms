5f6626ca49a306e6335e898b80bb279b948ad3de
dwyfrequency <jdwy215@gmail.com>
2019-06-11 09:34:57 -0400

    adding function args

---
+++ hackerrank/warmups/repeatingString/repeatingString.js
@@ -0,0 +1,3 @@
+// URL: https://www.hackerrank.com/challenges/repeated-string/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+
+function repeatedString(s, n) {}
