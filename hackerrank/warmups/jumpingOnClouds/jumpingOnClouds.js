f21d36cb77cc8d8a9782d73fea0db2f8dd38ce7a
dwyfrequency <jdwy215@gmail.com>
2019-06-10 22:07:32 -0400

    adding solution code

---
+++ hackerrank/warmups/jumpingOnClouds/jumpingOnClouds.js
@@ -0,0 +1,15 @@
+function jumpingOnClouds(c) {
+  let leftMostPointer = 0;
+  let stepCnt = 0;
+  while (leftMostPointer !== c.length - 1) {
+    if (c[leftMostPointer + 2] === 0) {
+      leftMostPointer += 2;
+    } else if (c[leftMostPointer + 1] === 0) {
+      leftMostPointer += 1;
+    }
+    stepCnt++;
+  }
+  return stepCnt;
+}
+
+console.log(jumpingOnClouds([0, 0, 1, 0, 0, 1, 0]));
