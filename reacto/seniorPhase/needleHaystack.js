730a451dfd2c9f33c5663453e0162b115cb62c26
dwyfrequency <jdwy215@gmail.com>
2019-06-12 13:42:43 -0400

    adding notes

--- reacto/seniorPhase/needleHaystack.js
+++ reacto/seniorPhase/needleHaystack.js
@@ -1,10 +1,20 @@
 // essentially we're creating an indexOf method
 // solution and problem description https://gist.github.com/a-Daley/31fcb2f35902689a15b888e8cd504b14
 
+/*
+  len 4 - len 2 = 2
+  ('jack', 'ck')
+  1. 'j' break
+  2. 'a' break
+  3. 'c' continue
+  4. 'ck' found
+*/
 const needleHaystack = (hayStackText, needleText) => {
+  // need to do less than or equal, or + 1 to b/c of a potential off by one error
+  // if we left it as '<' it would never reach the last index
   for (
     let hayStackIdx = 0;
-    hayStackIdx < hayStackText.length - needleText.length;
+    hayStackIdx <= hayStackText.length - needleText.length;
     hayStackIdx++
   ) {
     for (let needleIdx = 0; needleIdx < needleText.length; needleIdx++) {
@@ -18,4 +28,5 @@ const needleHaystack = (hayStackText, needleText) => {
   return -1;
 };
 
-console.log(needleHaystack('abbbaa'));
+console.log(needleHaystack('abbbaa', 'aba')); // -1
+console.log(needleHaystack('abbbaa', 'aa')); // 4
