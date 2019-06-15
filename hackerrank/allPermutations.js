b72553573cb5061a9dfb8efe233a4445696bba94
dwyfrequency <jdwy215@gmail.com>
2019-06-15 11:33:11 -0400

    adding more notes

--- hackerrank/allPermutations.js
+++ hackerrank/allPermutations.js
@@ -56,6 +56,7 @@ const getPermuRecurseV2 = str => {
 
 /*
 Sorting the str first means we only have to sort n characters vs. n! slots in array
+then call helper method above
 */
 const getPermsuOptomized = str => {
   return str
@@ -67,29 +68,32 @@ const getPermsuOptomized = str => {
 // console.log(getPermuRecurse('cat')); // ["act", "atc", "cat", "cta", "tac", "tca"]
 // console.log(getPermuRecurse('btt')); // ["btt", "tbt", "ttb"]
 
-// function stringPermutations(str) {
-//   let results = [];
-//   const letters = str.split('').sort();
-//   results.push([letters.shift()]);
-//   while (letters.length) {
-//     const currentLetter = letters.shift();
-//     const tempResults = [];
-//     // eslint-disable-next-line no-loop-func
-//     results.forEach(currResult => {
-//       for (let i = 0; i < currResult.length; i++) {
-//         const tmp = currResult.slice(); // make copy
-//         tmp.unshift(currentLetter);
-//         tempResults.push(tmp);
-//       }
-//     });
-//     results = tempResults;
-//   }
-//   return results
-//     .map(letterArr => letterArr.join(''))
-//     .filter((word, index, thisArr) => thisArr.indexOf(word) === index);
-// }
-
 function stringPermutations(str) {
+  let results = [];
+  const letters = str.split('').sort(); // sort our initial array so we can n is smaller
+  results.push([letters.shift()]);
+  while (letters.length) {
+    const currentLetter = letters.shift();
+    const tempResults = [];
+    // eslint-disable-next-line no-loop-func
+    results.forEach(currResult => {
+      for (let i = 0; i <= currResult.length; i++) {
+        const tmp = currResult.slice(); // make copy
+        // add the currentLetter at the i index of currResult
+        tmp.splice(i, 0, currentLetter);
+        // add the resulting tmp to our tempResults
+        tempResults.push(tmp);
+      }
+    });
+    // overwrite our results with tempResults created in forEach
+    results = tempResults;
+  }
+  return results
+    .map(letterArr => letterArr.join(''))
+    .filter((word, index, thisArr) => thisArr.indexOf(word) === index); // remove duplicates if there is a word with double letters
+}
+
+function stringPermutationsSortLast(str) {
   let results = [];
   let letters = str.split('');
   results.push([letters.shift()]); //add first letter (as an array) to results
