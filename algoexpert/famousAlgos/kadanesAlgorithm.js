9db1d69f6c94f4ae9636429246720093a0f7f639
dwyfrequency <jdwy215@gmail.com>
2019-06-16 10:06:36 -0400

    adding solution code

--- algoexpert/famousAlgos/kadanesAlgorithm.js
+++ algoexpert/famousAlgos/kadanesAlgorithm.js
@@ -4,9 +4,8 @@
  * So long as the total is above 0, we should always keep the continuous count.
  */
 // Time O(N) | O(1)
-function kadanesAlgorithmMine(array) {
+function kadanesAlgorithmMine(array, maxSum = array[0], total = array[0]) {
   // Write your code here.
-  let maxSum = (total = array[0]);
   for (let i = 1; i < array.length; i++) {
     if (total < 0) total = 0;
     total += array[i];
@@ -15,9 +14,12 @@ function kadanesAlgorithmMine(array) {
   return maxSum;
 }
 
-function kadanesAlgorithmAlgoExpert(array) {
+function kadanesAlgorithmAlgoExpert(
+  array,
+  maxSum = array[0],
+  total = array[0]
+) {
   // Write your code here.
-  let maxSum = (total = array[0]);
   for (let i = 1; i < array.length; i++) {
     const currNum = array[i];
     total = Math.max(total + currNum, currNum);
