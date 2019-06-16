966831765ffa6e47dda5fc60f1e11399dceaddb9
dwyfrequency <jdwy215@gmail.com>
2019-06-16 12:50:38 -0400

    adding initial attempt'

---
+++ algoexpert/dynamic/levenshteinDistance/levenshteinDistance.js
@@ -0,0 +1,18 @@
+function levenshteinDistance(str1, str2) {
+  // Write your code here.
+  const counter = {};
+  const lenDelta = Math.abs(str1.length - str2.length);
+  let totalChanges = lenDelta;
+  // let totalChanges = 0;
+  for (const char of str2) {
+    if (!counter[char]) counter[char] = 1;
+    else counter[char] += 1;
+  }
+  for (const char of str1) {
+    if (!counter[char]) totalChanges++;
+    else counter[char] = counter[char] === 1 ? null : counter[char] - 1;
+  }
+  return totalChanges;
+}
+
+console.log(levenshteinDistance('biting', 'mitten'));
