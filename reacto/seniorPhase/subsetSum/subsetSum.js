8f68059b8af4d4b69fa7604c7cc7c33881f546d1
dwyfrequency <jdwy215@gmail.com>
2019-06-28 20:49:03 -0400

    adding second set

--- reacto/seniorPhase/subsetSum/subsetSum.js
+++ reacto/seniorPhase/subsetSum/subsetSum.js
@@ -3,7 +3,8 @@
 const subsetSum = (target, arr) => {
   const possibleSums = new Set([0]);
   for (const num of arr) {
-    for (const sum of possibleSums) {
+    const currentPossibleSums = new Set(possibleSums);
+    for (const sum of currentPossibleSums) {
       const newSum = num + sum;
       if (newSum === target) return true;
       if (newSum < target) possibleSums.add(newSum);
@@ -12,4 +13,5 @@ const subsetSum = (target, arr) => {
   return false;
 };
 
-console.log(subsetSum(17, [1, 10, 5, 3]));
+console.log(subsetSum(17, [1, 10, 5, 3])); // false
+console.log(subsetSum(16, [1, 10, 5, 3])); // true
