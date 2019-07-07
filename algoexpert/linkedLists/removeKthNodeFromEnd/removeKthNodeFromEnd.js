2f67ef2c036b4a43e06ed394301f5ef828c96e93
dwyfrequency <jdwy215@gmail.com>
2019-07-07 06:59:06 -0400

    adding solution code

--- algoexpert/linkedLists/removeKthNodeFromEnd/removeKthNodeFromEnd.js
+++ algoexpert/linkedLists/removeKthNodeFromEnd/removeKthNodeFromEnd.js
@@ -1,21 +1,46 @@
+// URL: https://www.algoexpert.io/questions/Remove%20Kth%20Node%20From%20End
 // Time O(n) | Space
-function removeKthNodeFromEnd(head, k) {
+// function removeKthNodeFromEnd(head, k) {
+//   // Write your code here.
+//   // thinking two pointer solution - keep left most pointer
+//   // k nodes being the right and keep advancing until the end
+//   let curNode = head;
+//   let leftPnt = head,
+//     distance = 0;
+//   while (curNode.next) {
+//     if (distance === k) {
+//       leftPnt = leftPnt.next;
+//     } else {
+//       distance++;
+//     }
+//     curNode = curNode.next;
+//   }
+//   leftPnt.next = leftPnt.next.next;
+// }
+
+// URL: https://www.algoexpert.io/questions/Remove%20Kth%20Node%20From%20End
+// Time O(n) | Space
+function removeKthNodeFromEndAttempt2(head, k) {
   // Write your code here.
   // thinking two pointer solution - keep left most pointer
   // k nodes being the right and keep advancing until the end
   let curNode = head;
   let leftPnt = head,
-    distance = 0;
-  while (curNode.next) {
-    if (distance === k) {
-      leftPnt = leftPnt.next;
-    } else {
-      distance++;
-    }
+    counter = 1;
+  while (counter <= k) {
+    curNode = curNode.next;
+    counter++;
+  }
+  if (curNode === null) {
+    leftPnt.value = leftPnt.next.value;
+    leftPnt.next = leftPnt.next.next;
+    return;
+  }
+  while (curNode.next !== null) {
+    leftPnt = leftPnt.next;
     curNode = curNode.next;
   }
-  leftPnt.next = leftPnt.next.next;
 }
 
 // Do not edit the line below.
-exports.removeKthNodeFromEnd = removeKthNodeFromEnd;
+exports.removeKthNodeFromEnd = removeKthNodeFromEndAttempt2;
