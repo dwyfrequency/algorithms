375e20fe39a6e693164a63675979685403be85c4
dwyfrequency <jdwy215@gmail.com>
2019-07-12 17:10:47 -0400

    initial attempt at solution - needs debugging

--- algoexpert/arrays/hasSingleCycle/hasSingleCycle.js
+++ algoexpert/arrays/hasSingleCycle/hasSingleCycle.js
@@ -1,9 +1,19 @@
-/*
-
-*/
 function hasSingleCycle(array) {
   // Write your code here.
+  let stepPnt = 0,
+    curStepIdx = 0;
+  const idxSet = new Set();
+  while (stepPnt < array.length) {
+    const newStepIdx = (array[stepPnt] + curStepIdx) % array.length;
+    if (idxSet.has(newStepIdx)) return false;
+    idxSet.add(newStepIdx);
+    stepPnt++;
+    curStepIdx = newStepIdx;
+  }
+  return idxSet.length === array.length;
 }
+
+console.log(hasSingleCycle([0, 1, 1, 1, 1]));
 
 // Do not edit the line below.
 exports.hasSingleCycle = hasSingleCycle;
