630e886588f4fd667d88d0bf3fefb53d22c75a4c
dwyfrequency <jdwy215@gmail.com>
2019-07-17 22:01:42 -0400

    adding note on testing

--- algoexpert/search/searchSortedMatrix/searchInSortedMatrix.test.js
+++ algoexpert/search/searchSortedMatrix/searchInSortedMatrix.test.js
@@ -3,6 +3,8 @@ const {
   searchInSortedMatrix,
 } = require('./searchInSortedMatrix');
 
+/* RUN with jest searchInSortedMatrix.test.js  */
+
 describe('Binary Search Test', () => {
   test('should be return index of target', () => {
     expect(binarySearch([1, 3, 4, 5, 7], 3)).toBe(1);
