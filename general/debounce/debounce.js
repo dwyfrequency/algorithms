5d8e097c1af663d9ff9799c28d9810fa0063f614
dwyfrequency <jdwy215@gmail.com>
2019-07-18 13:16:17 -0400

    adding notes for debounce

--- general/debounce/debounce.js
+++ general/debounce/debounce.js
@@ -1,3 +1,9 @@
+/*
+Good description of debounce and throttle https://chrisboakes.com/how-a-javascript-debounce-function-works/
+We take in a function and the delay,
+we have a closure for our time, that we reset each time our function is called
+by resetting and clearing the timeout. Our function only runs when a user
+pauses. Good for scrolling effect or ajax call on search input */
 const debounce = (fn, delay) => {
   let timer;
   return function(...args) {
