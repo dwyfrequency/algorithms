26d290be3a98ac833b26e8118eeb68813ee789db
dwyfrequency <jdwy215@gmail.com>
2019-07-18 08:41:14 -0400

    adding notes

--- general/debounce/debounce.js
+++ general/debounce/debounce.js
@@ -1,9 +1,23 @@
 const debounce = (fn, delay) => {
   let timer;
   return function(...args) {
+    // need to use action function keyword here b/c if we're just using a
+    // basic input with vanilla js, the event listener binds the
+    // input to the func
     clearTimeout(timer);
     timer = setTimeout(() => {
       fn.apply(this, ...args);
     }, delay);
   };
 };
+
+const debouncedFunc = debounce(MyFunction, 500);
+
+document.getElementById('inputBox').addEventListener('keyup', debouncedFunc);
+
+function MyFunction() {
+  console.log('MyFunction', this);
+  console.log('MyFunction value', this.value);
+}
+
+// to test just create an html doc with <input id='inputBox' />
