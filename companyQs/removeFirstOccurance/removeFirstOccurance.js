6f798ff2175d552f1c7d8ab6902057793e9bd8e8
dwyfrequency <jdwy215@gmail.com>
2019-07-21 16:29:03 -0400

    adding solution code

--- companyQs/removeFirstOccurance/removeFirstOccurance.js
+++ companyQs/removeFirstOccurance/removeFirstOccurance.js
@@ -1,5 +1,7 @@
 const removeFirstOccurance = (arr, str) => {
+  // find the fist index of the str and set delete count to 1
   arr.splice(arr.indexOf(str), 1);
+  // return mutated array
   return arr;
 };
 
