c7f8d95da196578eee9e8f32301a8a913cceb8c8
dwyfrequency <jdwy215@gmail.com>
2019-07-20 21:38:14 -0400

    adding initial attempt

--- hackerrank/arrays/diagonalDifference.js
+++ hackerrank/arrays/diagonalDifference.js
@@ -1,5 +1,7 @@
 function diagonalDifference(arr) {
   // Write your code here
+  let rowDepth = arr.length;
+  let colDepth = arr[0].length;
   const leftRight = arr[0][0] + arr[1][1] + arr[2][2];
   const rightLeft = arr[0][2] + arr[1][1] + arr[2][0];
   console.log({ leftRight }, { rightLeft });
