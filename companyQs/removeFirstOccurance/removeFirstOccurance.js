a1d8fa3c4059a20683b4b29441455d019518c243
dwyfrequency <jdwy215@gmail.com>
2019-07-21 16:28:19 -0400

    adding solution code

--- companyQs/removeFirstOccurance/removeFirstOccurance.js
+++ companyQs/removeFirstOccurance/removeFirstOccurance.js
@@ -1,3 +1,6 @@
-const removeFirstOccurance = (arr, str) => {};
+const removeFirstOccurance = (arr, str) => {
+  arr.splice(arr.indexOf(str), 1);
+  return arr;
+};
 
 exports.removeFirstOccurance = removeFirstOccurance;
