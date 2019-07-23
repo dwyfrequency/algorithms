d8ac4169bf373e49101719c71fe3759a55d02566
dwyfrequency <jdwy215@gmail.com>
2019-07-23 15:52:01 -0400

    adding rotateLeftExtraSpaceFunction

--- leetcode/arrays/rotateRight.js
+++ leetcode/arrays/rotateRight.js
@@ -3,7 +3,9 @@
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var rotate = function(nums, k) {
+const rotateLeftExtraSpace = (nums, k) => {
   k = k % nums.length;
-  // nums.slice(-k).concat(nums.slice(0, -k)) // one solution
+  return nums.slice(-k).concat(nums.slice(0, -k)); // one solution
 };
+
+console.log(rotateLeftExtraSpace([1, 2, 3], 1));
