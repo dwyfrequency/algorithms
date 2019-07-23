d9e920f10a218c4eb2711b3dce567e349f39987e
dwyfrequency <jdwy215@gmail.com>
2019-07-23 15:29:24 -0400

    adding notes, time/space complexity

--- leetcode/arrays/removeDuplicates.js
+++ leetcode/arrays/removeDuplicates.js
@@ -1,11 +1,15 @@
 // URL: https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/727/
-// sorter(arr) -> int
+// sort(arr) -> int
+// Time O(n) | Space O(1)
 const removeDuplicates = arr => {
   // take sorted array and return length of unique nums
   for (let i = 1; i < arr.length; i++) {
     if (arr[i - 1] !== arr[i]) {
       continue;
     } else {
+      // b/c we are deleting the entry behind us, we need to reduce i
+      // or we might skip one of the letters
+      // our array is changing in length so our i will become offset
       arr.splice(i, 1);
       i--;
     }
