b385a1f72039865d6a90ef18cf9277728301eaa3
dwyfrequency <jdwy215@gmail.com>
2019-07-25 08:55:37 -0400

    adding solution code

---
+++ leetcode/arrays/intersection.js
@@ -0,0 +1,29 @@
+// eslint-disable-next-line complexity
+const intersection = (arr1, arr2) => {
+  const retArr = [];
+  const counter = {};
+  if (arr1.length > arr2.length) {
+    for (const val of arr2) {
+      if (counter[val] !== undefined) counter[val] += 1;
+      else counter[val] = 1;
+    }
+    for (const val of arr1) {
+      if (counter[val] > 0) {
+        retArr.push(val);
+        counter[val] -= 1;
+      }
+    }
+  } else {
+    for (const val of arr1) {
+      if (counter[val] !== undefined) counter[val] += 1;
+      else counter[val] = 1;
+    }
+    for (const val of arr2) {
+      if (counter[val] > 0) {
+        retArr.push(val);
+        counter[val] -= 1;
+      }
+    }
+  }
+  return retArr;
+};
