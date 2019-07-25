38c3b2ac8e5d6e6983051ed5f2e835d1adad0c55
dwyfrequency <jdwy215@gmail.com>
2019-07-25 14:42:48 -0400

    adding moveZeroes func

---
+++ leetcode/arrays/moveZeroes.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+  let leftPnt = 0,
+    rightPnt = 1;
+  while (rightPnt < nums.length) {
+    if (nums[leftPnt] !== 0) {
+      leftPnt++;
+      rightPnt++;
+    } else if (nums[leftPnt] === 0 && nums[rightPnt] === 0) {
+      rightPnt++;
+    } else {
+      nums[leftPnt] = nums[rightPnt];
+      nums[rightPnt] = 0;
+      leftPnt++;
+    }
+  }
+  return nums;
+};
