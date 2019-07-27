8aea29442e6a410724c5ae7b794dac49fc21def6
dwyfrequency <jdwy215@gmail.com>
2019-07-27 09:51:52 -0400

    adding slower solution but better space

--- leetcode/strings/firstUniqChar.js
+++ leetcode/strings/firstUniqChar.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 // Time O(n) | Space O(n/2)
-var firstUniqChar = function(s) {
+var firstUniqCharHashMap = function(s) {
   const hashMap = {};
   for (const ch of s) {
     if (hashMap[ch]) hashMap[ch] += 1;
@@ -11,6 +11,15 @@ var firstUniqChar = function(s) {
   }
   for (let i = 0; i < s.length; i++) {
     if (hashMap[s[i]] === 1) return i;
+  }
+  return -1;
+};
+
+// Time O(n^2) | Space O(1)
+var firstUniqChar = function(s) {
+  for (const ch of s) {
+    const idx = s.indexOf(ch);
+    if (idx === s.lastIndexOf(ch)) return idx;
   }
   return -1;
 };
