09ec492c1a8c5be364508555489d1de2f50a84d1
dwyfrequency <jdwy215@gmail.com>
2019-07-28 15:49:10 -0400

    adding solution code

--- leetcode/strings/isPalindrome.js
+++ leetcode/strings/isPalindrome.js
@@ -7,11 +7,12 @@ var isPalindrome = function(s) {
   let left = 0,
     right = s.length - 1;
   while (left < right) {
-    while (/^[a-z]/.test(s[left])) left++;
-    while (/^[a-z]/.test(s[right])) right--;
+    while (/[^a-z0-9]/.test(s[left])) left++;
+    while (/[^a-z0-9]/.test(s[right])) right--;
     if (s[left] !== s[right]) return false;
     left++;
     right--;
   }
   return true;
 };
+console.log(isPalindrome('A man, a plan, a canal: Panama'));
