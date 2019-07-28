b0f60cbea55365ee4735424bd2a3b85e0381b0d6
dwyfrequency <jdwy215@gmail.com>
2019-07-28 15:51:30 -0400

    adding big o

--- leetcode/strings/isPalindrome.js
+++ leetcode/strings/isPalindrome.js
@@ -2,11 +2,13 @@
  * @param {string} s
  * @return {boolean}
  */
+// Time O(n) | Space O(1)
 var isPalindrome = function(s) {
   s = s.toLowerCase();
   let left = 0,
     right = s.length - 1;
   while (left < right) {
+    // inner while's test to see if item is not alpha numberic, skip it
     while (/[^a-z0-9]/.test(s[left])) left++;
     while (/[^a-z0-9]/.test(s[right])) right--;
     if (s[left] !== s[right]) return false;
