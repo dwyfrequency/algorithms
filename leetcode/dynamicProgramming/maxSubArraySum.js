59be40691ff9a13629ff531563079aecc5f73301
dwyfrequency <jdwy215@gmail.com>
2019-07-30 15:09:07 -0400

    adding solution code

--- leetcode/dynamicProgramming/maxSubArraySum.js
+++ leetcode/dynamicProgramming/maxSubArraySum.js
@@ -2,13 +2,18 @@
  * @param {number[]} nums
  * @return {number}
  */
+// Time O(n) | Space O(1)
 var maxSubArray = function(nums) {
   let curMax = -Infinity;
   let curSum = -Infinity;
+  // Iterate through each number and calculate the current sum
+  // if it is less than zero, then we just want the max of the
+  // curSum and number
   for (const num of nums) {
     if (curSum < 0) {
       curSum = Math.max(num, curSum);
     } else {
+      // if the num is greater than just add it to the curSum
       curSum += num;
     }
     curMax = Math.max(curSum, curMax);
