fa335e16570ab617f71122e70826ebb0079337a0
dwyfrequency <jdwy215@gmail.com>
2019-07-30 09:51:03 -0400

    adding memoized solution

--- leetcode/dynamicProgramming/climbStairs.js
+++ leetcode/dynamicProgramming/climbStairs.js
@@ -12,13 +12,15 @@ var climbStairsTooSlow = function(n) {
   return climbStairs(n - 1) + climbStairs(n - 2);
 };
 
+/**
+ * @param {number} n
+ * @return {number}
+ */
 var climbStairs = function(n, memo = {}) {
   if (n === 0) return 1;
   if (n < 0) return 0;
-  if (memo[n - 1] === undefined) {
-    memo[n - 1] = climbStairs(n - 1);
-  } else if (memo[n - 1] === undefined) {
-    memo[n - 2] = climbStairs(n - 2);
+  if (memo[n] === undefined) {
+    memo[n] = climbStairs(n - 1, memo) + climbStairs(n - 2, memo);
   }
-  return memo[n - 1] + memo[n - 2];
+  return memo[n];
 };
