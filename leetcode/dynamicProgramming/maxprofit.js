afec68716cfb386e2bc0b928783fde44600fd2f6
dwyfrequency <jdwy215@gmail.com>
2019-07-30 13:26:45 -0400

    adding naive solution

--- leetcode/dynamicProgramming/maxprofit.js
+++ leetcode/dynamicProgramming/maxprofit.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 // O(n^2) | Space O(1)
-var maxProfit = function(prices) {
+var maxProfitSlow = function(prices) {
   let curMax = 0;
   for (let i = 0; i < prices.length - 1; i++) {
     for (let k = i + 1; k < prices.length; k++) {
