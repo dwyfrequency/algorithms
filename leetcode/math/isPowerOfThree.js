c90d0e31e60b6c146ea1fd5529f61c102cc2c19a
dwyfrequency <jdwy215@gmail.com>
2019-07-29 20:19:05 -0400

    adding time and space

--- leetcode/math/isPowerOfThree.js
+++ leetcode/math/isPowerOfThree.js
@@ -3,7 +3,9 @@
  * @return {boolean}
  */
 var isPowerOfThree = function(n) {
-  let total = 1;
+  let total = 1; // start total with 1 b/c 3^0 === 1
+  // keep multiplying total by three until it is no longer less than n
   while (total < n) total *= 3;
+  // return if total equals n, this tells us if is a power of 3
   return total === n;
 };
