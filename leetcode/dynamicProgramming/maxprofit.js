32af01e6c065bc2b620d4a71237767302b2b3434
dwyfrequency <jdwy215@gmail.com>
2019-07-30 13:23:27 -0400

    adding solution

---
+++ leetcode/dynamicProgramming/maxprofit.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+// O(n^2) | Space O(1)
+var maxProfit = function(prices) {
+  let curMax = 0;
+  for (let i = 0; i < prices.length - 1; i++) {
+    for (let k = i + 1; k < prices.length; k++) {
+      curMax = Math.max(prices[k] - prices[i], curMax);
+    }
+  }
+  return curMax;
+};
