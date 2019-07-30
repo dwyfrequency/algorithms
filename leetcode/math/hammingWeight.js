d895df3da0793142f83256272fc2c5014ba1d3f7
dwyfrequency <jdwy215@gmail.com>
2019-07-30 18:07:12 -0400

    Adding notes

--- leetcode/math/hammingWeight.js
+++ leetcode/math/hammingWeight.js
@@ -5,7 +5,7 @@
 // Time O(2n) | Space O(n)
 var hammingWeight = function(n) {
   return n
-    .toString(2)
-    .split('')
-    .filter(x => x === '1').length;
+    .toString(2) // convert number to binary
+    .split('') // split resulting num to str
+    .filter(x => x === '1').length; // create array of 1s
 };
