c55a1ef1824caf03bb706d1ec70c434f8768f987
dwyfrequency <jdwy215@gmail.com>
2019-07-30 21:05:33 -0400

    adding solution and time/space

---
+++ leetcode/strings/strStr.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStrEasiest = function(haystack, needle) {
+  return haystack.indexOf(needle);
+};
+
+// eslint-disable-next-line complexity
+var strStrBlem = function(haystack, needle) {
+  if (haystack.length < needle.length) return -1;
+  if (!needle || haystack === needle) return 0;
+  for (let i = 0; i <= haystack.length - needle.length; i++) {
+    if (haystack.slice(i, needle.length) === needle) return i;
+  }
+  return -1;
+};
+
+// Time O(h*n) | Space O(h/n)
+var strStr = function(haystack, needle) {
+  if (haystack.length < needle.length) return -1;
+  if (!needle || haystack === needle) return 0;
+  for (let i = 0; i <= haystack.length - needle.length; i++) {
+    if (haystack[i] === needle[0]) {
+      const slice = haystack.slice(i, i + needle.length);
+      if (slice === needle) return i;
+    }
+  }
+  return -1;
+};
+
+console.log(strStr('hello', 'll'));
