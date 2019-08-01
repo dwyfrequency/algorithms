430fdcf2ffad221215b12d2c4be6c56c67518801
dwyfrequency <jdwy215@gmail.com>
2019-07-31 21:21:22 -0400

    adding rules for algo

---
+++ companyQs/strings/abbrevGenerator.js
@@ -0,0 +1,13 @@
+// [book, cat, interveel, interveal, lookook, loooook]
+// [b2k, cat, interveel, interveal, look2k,looo2k]
+
+/* RULES
+1. abbrev has to be shorter than the original else return orig
+2. abbrev will include the number of characters between the start and end
+3. if two abbrevs conflict, generate a new abbrev that conforms. (make sure adheres to rule 1 & 2)
+*/
+
+const abbrevGenerator = arr => {
+  const abbrevReturnArr = [];
+  for (let i = 0; i < arr.length; i++) {}
+};
