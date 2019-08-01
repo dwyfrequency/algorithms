b829899f01fd1ab48d1a5ad3a45a44cf45d53d39
dwyfrequency <jdwy215@gmail.com>
2019-07-31 21:36:59 -0400

    adding abbrev generator

--- companyQs/strings/abbrevGenerator.js
+++ companyQs/strings/abbrevGenerator.js
@@ -6,8 +6,46 @@
 2. abbrev will include the number of characters between the start and end
 3. if two abbrevs conflict, generate a new abbrev that conforms. (make sure adheres to rule 1 & 2)
 */
+const resolveAbbrevConflict = (str1, str2) => {
+  let counter = 1;
+  while (str1[counter] === str2[counter]) counter++;
+  // have to add 2 b/c counter 0 is the first char so we need to add 1,
+  // and add another 1 b/c of the last ch in the str
+  const delta = counter + 2;
+  const str1Abbrev = str1.slice(0, counter + 1) + delta + str1.slice(-1);
+  const str2Abbrev = str2.slice(0, counter + 1) + delta + str2.slice(-1);
+  return str1.length === str1Abbrev.length
+    ? [str1, str2]
+    : [str1Abbrev, str2Abbrev];
+};
+
+const generateNewAbbrev = str => {
+  // minus 2, because we have one character for the start and one for the end
+  // leaving the len -2 as the remaining char len
+  const delta = str.length - 2;
+  return str[0] + delta + str[str.length - 1];
+};
 
 const abbrevGenerator = arr => {
   const abbrevReturnArr = [];
-  for (let i = 0; i < arr.length; i++) {}
+  const hashMap = {};
+  for (let i = 0; i < arr.length; i++) {
+    const currStr = arr[i];
+    if (currStr <= 3) {
+      abbrevReturnArr.push(currStr);
+    } else {
+      const curAbbrev = generateNewAbbrev(arr[i]);
+      if (hashMap[curAbbrev] !== undefined) {
+        const oldIdx = hashMap[curAbbrev];
+        const [newPrevAbbrev, newCurAbbrev] = resolveAbbrevConflict(
+          arr[oldIdx],
+          arr[i]
+        );
+      } else {
+        abbrevReturnArr.push(curAbbrev);
+        hashMap[curAbbrev] = i;
+      }
+    }
+  }
+  return abbrevReturnArr;
 };
