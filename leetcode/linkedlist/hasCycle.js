5b5be5fc764f58177d2fba86180bfe9b0828959a
dwyfrequency <jdwy215@gmail.com>
2019-08-10 15:25:06 -0400

    Adding solution code

--- leetcode/linkedlist/hasCycle.js
+++ leetcode/linkedlist/hasCycle.js
@@ -10,12 +10,25 @@
  * @param {ListNode} head
  * @return {boolean}
  */
+// Time O(n) | Space O(n)
 var hasCycle = function(head) {
   const set = new Set();
   while (head) {
     if (set.has(head)) return true;
     set.add(head);
     head = head.next;
+  }
+  return false;
+};
+
+const hasCycleTwoPointers = head => {
+  if (!head || !head.next) return false;
+  let slowPnt = head;
+  let fastPnt = head.next;
+  while (fastPnt && fastPnt.next) {
+    if (slowPnt === fastPnt) return true;
+    slowPnt = slowPnt.next;
+    fastPnt = fastPnt.next.next;
   }
   return false;
 };
---
+++ leetcode/searching/isBadVersion.js
@@ -0,0 +1,1 @@
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/96/sorting-and-searching/774/
