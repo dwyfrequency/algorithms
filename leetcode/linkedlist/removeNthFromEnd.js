f25bf9fa55ba3e475d25a8fc29b422474f83736b
dwyfrequency <jdwy215@gmail.com>
2019-08-09 21:32:26 -0400

    adding solution code

--- leetcode/linkedlist/removeNthFromEnd.js
+++ leetcode/linkedlist/removeNthFromEnd.js
@@ -14,14 +14,23 @@ var removeNthFromEnd = function(head, n) {
   let leftMostPnt = head;
   let leadPnt = head;
   for (let i = 0; i < n; i++) leadPnt = leadPnt.next;
-  if (!leadPnt) {
-    head.val = head.next.val;
-    head = head.next;
+  // if they are asking us to remove the head
+  if (leadPnt === null) {
+    // if only one element, make head null
+    if (!head.next) {
+      head = null;
+    } else {
+      // update head to next value in list
+      head.val = head.next.val;
+      head = head.next;
+    }
     return head;
   }
+  // iterate until the tail
   while (leadPnt.next !== null) {
     leadPnt = leadPnt.next;
     leftMostPnt = leftMostPnt.next;
   }
-  leadPnt.next = leadPnt.next.next;
+  leftMostPnt.next = leftMostPnt.next.next;
+  return head;
 };
