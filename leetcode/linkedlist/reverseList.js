c80f3d6e9d742e6d4fae1dd9efa5f6d8d3020f4c
dwyfrequency <jdwy215@gmail.com>
2019-08-09 23:38:02 -0400

    attempted

---
+++ leetcode/linkedlist/reverseList.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+  if (!head || !head.next) return head;
+  let prevNode = null,
+    curNode = head,
+    nextNode = curNode.next;
+  while (nextNode) {
+    curNode.next = prevNode;
+    prevNode = curNode;
+    curNode = nextNode;
+    nextNode = nextNode.next;
+  }
+  console.log({ curNode });
+  return curNode;
+};
