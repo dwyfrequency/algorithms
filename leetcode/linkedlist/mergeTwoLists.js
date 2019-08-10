ea852695e44704a2b66d79910b79626a114bca6a
dwyfrequency <jdwy215@gmail.com>
2019-08-10 12:55:25 -0400

    adding solution and notes

---
+++ leetcode/linkedlist/mergeTwoLists.js
@@ -0,0 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+class ListNode {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+var mergeTwoLists = function(l1, l2) {
+  const head = new ListNode(); // keep pointer to head
+  let curNode = head;
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      curNode = new ListNode(l1.val);
+      l1 = l1.next;
+    } else {
+      curNode = new ListNode(l2.val);
+      l2 = l2.next;
+    }
+    curNode = curNode.next;
+  }
+  curNode.next = l1 || l2;
+  /*
+  ListNode {
+  val: undefined,
+  next: ListNode { val: 1,
+                  next: ListNode { val: 2, next: [ListNode] } }}
+  */
+  // because this was just a unpopulated node w/ val, the next value actually
+  // points to our list, see above
+  return head.next;
+};
+
+const l1 = new ListNode(1);
+const l2 = new ListNode(2);
+const l3 = new ListNode(3);
+const l4 = new ListNode(4);
+const l5 = new ListNode(5);
+l1.next = l2;
+l2.next = l3;
+l3.next = l4;
+l4.next = l5;
+const linkedList = l1;
+console.log(mergeTwoLists(linkedList));
