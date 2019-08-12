1f831c6f12c93f779060d69f73ecd1295e7b8665
dwyfrequency <jdwy215@gmail.com>
2019-08-12 12:30:10 -0400

    adding solution code w/ notes

--- leetcode/linkedlist/swapPairs.js
+++ leetcode/linkedlist/swapPairs.js
@@ -11,10 +11,16 @@
  */
 var swapPairs = function(head) {
   if (!head) return null;
+  // if odd number of nodes, there is nothing to swap with
   if (!head.next) return head;
+  // get the next node
   const node2 = head.next;
+  // get reference to the node in front of node2
   const node1NewNext = node2.next;
+  // reassign next node.next to head
   node2.next = head;
+  // swap the pairs ahead and assign to our trailing node
   head.next = swapPairs(node1NewNext);
+  // return new start node
   return node2;
 };
