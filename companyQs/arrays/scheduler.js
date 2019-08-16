ec0f7ded717d28d56359105941079f5f75508a16
dwyfrequency <jdwy215@gmail.com>
2019-08-15 21:13:08 -0400

    adding problem

---
+++ companyQs/arrays/scheduler.js
@@ -0,0 +1,9 @@
+/*
+find intersection of times
+[startTime, endTime]
+agent [[1, 4], [8,9],[20, 24]]
+customer [[2, 3], [7,10], [21, 23]]
+-> [[2,3], [8,9], [21,23]]
+*/
+// ([arr], [arr]) -> [arr]
+const scheduler = (agent, client) => {};
