8aa871cc8b7f03e5394b7792b3fc0715fed72dc0
dwyfrequency <jdwy215@gmail.com>
2019-08-15 21:20:31 -0400

    adding solution code

--- companyQs/arrays/scheduler.js
+++ companyQs/arrays/scheduler.js
@@ -4,6 +4,25 @@ find intersection of times
 agent [[1, 4], [8,9],[20, 24]]
 customer [[2, 3], [7,10], [21, 23]]
 -> [[2,3], [8,9], [21,23]]
+
+agent [[1, 20]]
+customer [[2, 3], [7,10], [21, 23]]
+-> [[2,3], [7,10], [21,23]]
+
 */
+// Time O(n + m) | Space O(m)
 // ([arr], [arr]) -> [arr]
-const scheduler = (agent, client) => {};
+const scheduler = (agent, client) => {
+  let agentPnt = 0,
+    clientPnt = 0;
+  const returnArr = [];
+  while (agentPnt < agent.length && clientPnt < client.length) {
+    const [agentStartTime, agentEndTime] = agentPnt;
+    const [clientStartTime, clientEndTime] = clientPnt;
+    const maxStart = Math.max(agentStartTime, clientStartTime);
+    const minEnd = Math.min(agentEndTime, clientEndTime);
+    if (maxStart < minEnd) returnArr.push([maxStart, minEnd]);
+    if (agentEndTime === minEnd) agentPnt++;
+    else clientPnt++;
+  }
+};
